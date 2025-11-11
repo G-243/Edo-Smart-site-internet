e5fbf719e8f107a7bf18b0299168bf7d1a974a49
Gloria kizowa <gloriakiz2003@gmail.com>
2025-11-11 17:05:41 +0200

    Description des changements

--- components/Logo.js
+++ components/Logo.js
@@ -4,12 +4,12 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="group cursor-pointer transition-transform duration-300 hover:scale-105">
-      <div className="relative w-48 md:w-40 h-16 md:h-28">
+      <div className="relative w-48 md:w-30 h-16 md:h-28">
         <Image
           src="https://res.cloudinary.com/ds4x7wbbv/image/upload/v1761648041/1761647853532_h4glnk.png"
           alt="Smart DRC Photography Logo"
           fill
-          className="object-contain w-32 md:w-48"
+          className="object-contain"
           priority
         />
       </div>
